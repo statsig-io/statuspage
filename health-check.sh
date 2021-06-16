94f1bd07f8e04b2ce1bdace68d933c8a56ff8d2a
Rodrigo Roim <roim@statsig.com>
2021-06-16 14:21:15 -0700

    Stop pushing status updates in the origin repository (#10)
    * Stop pushing status updates in the origin repository
    
    * bash syntax fix
    
    * Prettify preview runs
    
    * Remove test entries in GH workflow
    
    * Add status badge to README
    
    Co-authored-by: Vijaye Raji <vijaye@statsig.com>

--- health-check.sh
+++ health-check.sh
@@ -1,23 +1,28 @@
+# In the original repository we'll just print the result of status checks,
+# without committing. This avoids generating several commits that would make
+# later upstream merges messy for anyone who forked us.
+commit=true
+origin=$(git remote get-url origin)
+if [[ $origin == *statsig-io/statuspage* ]]
+then
+  commit=false
+fi
+
 KEYSARRAY=()
 URLSARRAY=()
 
 urlsConfig="./urls.cfg"
+echo "Reading $urlsConfig"
 while read -r line
 do
-  echo "$line"
+  echo "  $line"
   IFS='=' read -ra TOKENS <<< "$line"
-  echo $TOKENS
-
   KEYSARRAY+=(${TOKENS[0]})
   URLSARRAY+=(${TOKENS[1]})
 done < "$urlsConfig"
 
-
 echo "***********************"
-echo $KEYSARRAY
-echo $URLSARRAY
-echo ${#KEYSARRAY[@]}
-echo "***********************"
+echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
 
 mkdir -p logs
 
@@ -25,9 +30,7 @@ for (( index=0; index < ${#KEYSARRAY[@]}; index++))
 do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
-
-  echo $key
-  echo $url
+  echo "  $key=$url"
 
   for i in 1 2 3 4; 
   do
@@ -43,12 +46,20 @@ do
     sleep 5
   done
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  echo $dateTime, $result >> "logs/${key}_report.log"
+  if [[ $commit == true ]]
+  then
+    echo $dateTime, $result >> "logs/${key}_report.log"
+  else
+    echo "    $dateTime, $result"
+  fi
 done
 
-git config --global user.name 'Vijaye Raji'
-git config --global user.email 'vijaye@statsig.com'
-git add -A --force logs/
-git commit -am 'Status logs - automated'
-git push
-
+if [[ $commit == true ]]
+then
+  # Let's make Vijaye the most productive person on GitHub.
+  git config --global user.name 'Vijaye Raji'
+  git config --global user.email 'vijaye@statsig.com'
+  git add -A --force logs/
+  git commit -am '[Automated] Update Health Check Logs'
+  git push
+fi
