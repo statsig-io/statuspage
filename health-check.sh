ba886e1ecdf5e4acd378956725e842b6151c063f
Izumi Sena Sora <61591769+IzumiSena@users.noreply.github.com>
2021-12-19 06:26:16 +0000

    allow response http status 302 by default (#15)

--- health-check.sh
+++ health-check.sh
@@ -35,7 +35,7 @@ do
   for i in 1 2 3 4; 
   do
     response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
       result="success"
     else
       result="failed"
