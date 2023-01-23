8e4da9b9e24c1e95fe2068fef0665a424d0cee98
vcvfgrssrfe <115990504+vcvfgrssrfe@users.noreply.github.com>
2023-01-22 23:35:47 -0800

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -59,8 +59,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'vcvfgrssrfe'
+  git config --global user.email '115990504+vcvfgrssrfe@users.noreply.github.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
