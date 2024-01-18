16637071d34ef22a6ec358e5b89ea89fde11793e
Martin Trigaux <me@mart-e.be>
2024-01-18 15:18:57 +0100

    Don't overwrite user git config
    Users need to have a git configured to be able to use git push so no need to
    force a name and email. This avoid unfortunate overwrite of user config.

--- health-check.sh
+++ health-check.sh
@@ -58,9 +58,6 @@ done
 
 if [[ $commit == true ]]
 then
-  # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
